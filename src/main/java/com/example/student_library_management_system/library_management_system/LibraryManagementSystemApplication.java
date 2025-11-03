a0ee0d315b1075d53bc8c8815b50964efe58b791
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-11-03 15:29:27 +0530

    build an project student-library-management-system

---
+++ src/main/java/com/example/student_library_management_system/library_management_system/LibraryManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.student_library_management_system.library_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+		System.out.println("spring project run sussessfully");
+	}
+
+}
